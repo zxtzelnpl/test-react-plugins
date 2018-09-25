5e63bce5baad49022aaa2c6f0cbfb70a782cea94
was <zxt_zel_npl@163.com>
2018-09-25 14:08:20 +0800

    no

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -25,4 +25,6 @@ export class Hello extends React.Component<HelloProps, {}> {
 
         return <h1 className={className}>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
+
+    
 }
